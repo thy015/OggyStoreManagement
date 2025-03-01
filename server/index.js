363fc4bb997ef9f8c21c5752a6ddf48814cfd26a
Thy Chan <thymai.1510@gmail.com>
2025-03-01 20:59:38 +0700

    fix: add link to cors
    Signed-off-by: Thy Chan <thymai.1510@gmail.com>

--- server/index.js
+++ server/index.js
@@ -11,42 +11,55 @@ const http = require('http');
 const admin = require('firebase-admin');
 const serviceAccount = require('./serviceAccount.json');
 const authenRouter = require('./apis/authens/authen.controller');
+const receiptRouter = require ('./apis/receipts/receipt.controller');
 
+const allowedOrigins = [
+  'http://localhost:8081',
+  'https://oggy-store-management-be.vercel.app'
+];
+
+app.use(
+    cors ({
+      origin: (origin, callback) => {
+        if (!origin) {
+          return callback (null, true);
+        }
+        if (
+            allowedOrigins.some ((allowedOrigin) => origin.startsWith (allowedOrigin))
+        ) {
+          return callback (null, true);
+        }
+        console.error ('Blocked by CORS:', origin);
+        callback (new Error ('Not allowed by CORS'));
+      },
+      credentials: true,
+    })
+);
 // middleware always put first
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(morgan('combined'));
+app.use (bodyParser.json ());
+app.use (cookieParser ());
+app.use (morgan ('combined'));
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+admin.initializeApp ({
+  credential: admin.credential.cert (serviceAccount),
   databaseURL: 'https://lab09-23f9e-default-rtdb.firebaseio.com',
 });
 
 //route
-app.use('/api/v1/authens/', authenRouter);
-
-const allowedOrigins = ['http://localhost:8081'];
-
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) {
-        return callback(null, true);
-      }
-      if (
-        allowedOrigins.some((allowedOrigin) => origin.startsWith(allowedOrigin))
-      ) {
-        return callback(null, true);
-      }
-      console.error('Blocked by CORS:', origin);
-      callback(new Error('Not allowed by CORS'));
-    },
-    credentials: true,
-  })
-);
-// swagger config
-
+app.use ('/api/v1/authens', authenRouter);
+app.use ('/api/v1/receipts', receiptRouter);
 app.use(express.static('public'));
+
+// 🔥 Add CORS headers manually in case middleware fails
+app.use ((req, res, next) => {
+  res.header ('Access-Control-Allow-Origin', 'http://localhost:8081');
+  res.header ('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header ('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header ('Access-Control-Allow-Credentials', 'true');
+  next ();
+});
+
+app.get ("/", (req, res) => res.send ("Express on Vercel"));
 
 // Xử lý lỗi
 app.use((err, req, res, next) => {
